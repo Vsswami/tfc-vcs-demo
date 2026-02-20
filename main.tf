9779bb30cdb796372bc10d3e7837b2be0943150e
vansh <vsswami431@gmail.com>
2026-02-20 19:54:46 +0530

    first commit

---
+++ main.tf
@@ -0,0 +1,45 @@
+terraform {
+
+  required_providers {
+
+    aws = {
+
+      source  = "hashicorp/aws"
+
+      version = "~> 5.0"
+
+    }
+
+  }
+
+ 
+
+  cloud {
+
+    organization = "Vansh-organization"
+
+    workspaces {
+
+      name = "vansh-tfs-demo"
+
+    }
+
+  }
+
+}
+
+ 
+
+provider "aws" {
+
+  region = "us-east-1"
+
+}
+
+ 
+
+resource "aws_s3_bucket" "demo" {
+
+  bucket = "tfc-cli-demo-vansh-bucket-123"
+
+}
